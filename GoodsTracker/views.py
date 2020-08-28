fd9e7332a9814d9e2fe6db2efafacbef293e213e
Vlad <vl4d.tikhom@yandex.ru>
2020-08-28 17:02:34 +0500

    Добавлен вывод товаров

--- GoodsTracker/views.py
+++ GoodsTracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import Product
+
+class ProductViewer(View):
+
+    def get(self, request):
+        products = Product.objects.all()
+        return render(request, 'GoodsTracker/product_list.html', {'product_list': products})
